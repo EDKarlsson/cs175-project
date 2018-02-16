f66a9bc50c43cc770a5f89e8b480dfd90cd4bb0c
John <john@alvstad.net>
2018-02-15 20:27:08 -0800

    added word frequencies of all articles

--- src/dataprocessing/preprocessing.py
+++ src/dataprocessing/preprocessing.py
@@ -2,7 +2,7 @@ import pandas as pd
 from scipy.sparse import csr_matrix
 import os
 import nltk
-
+import pickle
 
 def load_data():
     DATA_DIR = "../../data"
@@ -12,7 +12,7 @@ def load_data():
 def bag_of_words(article):
     tokens = nltk.word_tokenize(article)
     fdist = nltk.FreqDist(tokens)
-    #remove_stopwords(fdist)
+    remove_stopwords(fdist)
     return fdist
 
 
@@ -26,11 +26,9 @@ def remove_stopwords(tokens, stopwords=set(nltk.corpus.stopwords.words('english'
 if __name__ == '__main__':
     data = load_data()
     # fdist = bag_of_words(data['content'][0])
-    count = 0
     fdist = nltk.FreqDist()
-    for article in data['content']:
-        fdist |= bag_of_words(article)
-        if (count % 1000) == 0:
-            print(count)
-        count += 1
-    remove_stopwords(fdist)
+    for i, article in enumerate(data['content']):
+        fdist += bag_of_words(article)
+        if i % 1000 == 0:
+            print(i)
+    pickle.dump(fdist, open('../../out/all_articles.txt', mode = 'wb'))
