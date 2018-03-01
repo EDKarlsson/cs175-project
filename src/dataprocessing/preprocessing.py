c3e9b3740e2b2c75c1e1ad58156bbe15e4d90fc0
edkarlsson <Erikdkarlsson8@gmail.com>
2018-02-28 19:44:53 -0800

    Made updates to preprocessing. Need to figure out loading the data into a matrix.

--- src/dataprocessing/preprocessing.py
+++ src/dataprocessing/preprocessing.py
@@ -3,11 +3,19 @@ from scipy.sparse import csr_matrix
 import os
 import nltk
 import pickle
+import unicodedata
+import numpy as np
+import scipy
+
+global CRAP_CHAR
+CRAP_CHAR = 0
+
 
 def load_data():
     DATA_DIR = "../../data"
     ARTICLES = os.listdir(DATA_DIR)
     return pd.concat([pd.read_csv(DATA_DIR + "/" + f) for f in ARTICLES if '.csv' in f], ignore_index=True)
+
 
 def bag_of_words(article):
     tokens = nltk.word_tokenize(article)
@@ -22,13 +30,50 @@ def remove_stopwords(tokens, stopwords=set(nltk.corpus.stopwords.words('english'
             del tokens[key]
 
 
+def save_char_dict():
+    data = load_data()
+    articles = data['content'][:200]
+
+    st = set()
+    asciiset = set()
+    for a in articles:
+        st.update({l for l in a if ord(l) < 128})
+        asciiset.update({ord(l) for l in a if ord(l) < 128})
+
+    print(st)
+    st = sorted(st)
+    d = {c: st.index(c) for c in st}
+    print(d)
+    pickle.dump(d, open('../../out/character_dict', mode='wb'))
+
+
+def bin_matrix():
+    data = load_data()
+    article_embedding = []
+    for a in data['content']:
+        article_embedding += [ord(c) for c in a if ord(c) < 128]
+
+    # print(mat)
+    pickle.dump(article_embedding, open('../../out/article_embedding_long', mode='wb'))
+
+    # return article_embedding
+
+
+def load_article_embedding(type="long"):
+    return pickle.load(open('../../data/article_embedding_{}'.format(type), mode='rb'))
+
 
 if __name__ == '__main__':
-    data = load_data()
-    # fdist = bag_of_words(data['content'][0])
-    fdist = nltk.FreqDist()
-    for i, article in enumerate(data['content']):
-        fdist += bag_of_words(article)
-        if i % 1000 == 0:
-            print(i)
-    pickle.dump(fdist, open('../../out/all_articles.txt', mode = 'wb'))
+    print('derp')
+
+    bin_matrix()
+    # load_article_embedding()
+
+    # data = load_data()
+    # # fdist = bag_of_words(data['content'][0])
+    # fdist = nltk.FreqDist()
+    # for i, article in enumerate(data['content']):
+    #     fdist += bag_of_words(article)
+    #     if i % 1000 == 0:
+    #         print(i)
+    # pickle.dump(fdist, open('../../out/all_articles.txt', mode = 'wb'))
---
+++ src/models/kmodel.py
@@ -0,0 +1,20 @@
+import keras
+import src.dataprocessing.preprocessing as preproc
+import numpy as np
+
+article_embedding = preproc.load_article_embedding()
+article_embedding = np.array(article_embedding)
+
+Xtr = article_embedding[:-1]
+Ytr = article_embedding[1:]
+
+model = keras.Sequential()
+# Each add is a layer
+model.add(keras.layers.Embedding(128, 128))  # Embedding layer
+model.add(keras.layers.LSTM(1024))
+model.add(keras.layers.Dense(512, activation='sigmoid'))
+model.add(keras.layers.Dense(128, activation='sigmoid'))
+model.add(keras.layers.Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+model.summary()
