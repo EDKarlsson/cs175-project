8fd83e834b159019765e4bfa2e9af9f981a6746e
John Alvstad <john@alvstad.net>
2018-03-01 16:03:45 -0800

    got lstm working

--- src/models/kmodel.py
+++ src/models/kmodel.py
@@ -1,5 +1,7 @@
+import sys
+sys.path.append('..')
 import keras
-import src.preprocessing as preproc
+import preprocessing as preproc
 import numpy as np
 
 x_train, y_train = preproc.make_sequences(1000)
@@ -9,13 +11,13 @@ h1_size = 100
 epochs = 10
 
 
-model = keras.Sequential()
+model = keras.models.Sequential()
 # Each add is a layer
 model.add(keras.layers.Embedding(preproc.NUM_VOCAB, h1_size, input_length=1))  # Embedding layer
 model.add(keras.layers.LSTM(1024))
 model.add(keras.layers.Dense(preproc.NUM_VOCAB, activation='softmax'))
 
-model.compile(loss='categorical_crossentropy', optimizer='adam')
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam')
 model.summary()
 
 model.fit(x_train, y_train, verbose=1, epochs=epochs)
